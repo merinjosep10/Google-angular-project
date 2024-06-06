d1d24043f9eeb1ea40b3cad99742fd237995ff56
merinjosep10 <merin.jose@people10.com>
2024-06-06 14:34:15 +0530

    Project is added to git

---
+++ src/app/center/center.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-center',
+    standalone: true,
+    templateUrl: './center.component.html',
+    styleUrl: './center.component.css'
+})
+
+export class CenterComponent {}
